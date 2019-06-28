094f81c40aff435f6f39cded408e198d1bfd4b1c
MichaelCastillo <michael.castillo.p96@gmail.com>
2019-06-28 11:18:56 -0400

    feat: Anade tests unitarios terminados

--- models/Article.js
+++ models/Article.js
@@ -2,6 +2,8 @@
 const mongoose = require('mongoose');
 var ArticleSchema = new mongoose.Schema({
     name: String,
+    category: String,
+    price: Number,
 })
 
 module.exports = mongoose.model('Article', ArticleSchema);
--- test/Articles.test.js
+++ test/Articles.test.js
@@ -2,26 +2,29 @@ var app =  require('../app');
 var Article = require('../models/Article')
 const assert = require('assert');
 
+
+const testingValues = [
+    {name:"Naranja", price:1000, category:"frutas"},
+    {name:"Pepsi", price:3000, category:"bebida"},
+    {name:"Jabón", price:4000, category:"baño"}
+]
+
 describe('Articles', ()=>{
 
     describe('Creación', () =>{
+
+        beforeEach((done)=>{
+            //Se borran todos los documentos
+            Article.deleteMany({}).then(async ()=>{
+                for(let i = 0; i<testingValues.length; i++){
+                    let article = new Article(testingValues[i])
+                    await article.save()
+                }
+                done();
+            })
+        })
+
         it('Crear un artículo', (done)=>{
-
-            /*
-            let article = new Article({name:"test"})
-            try{
-
-                await article.save()
-                Article.findOne({name:'test2'})
-                            .then(res => {
-                                assert(res.name == 'test');
-                        })
-            }catch(e){
-                console.log(e)
-            }
-            */
-
-            
             var article = new Article({name:"test"})
             article.save()
             .then(res =>{
@@ -33,10 +36,27 @@ describe('Articles', ()=>{
                     console.log(e)
                     done();
                 })
-                
             })
-            
-            
+        })
+
+        it('Validando creación de articulos, deben ser tres.', (done) =>{
+            let articles;
+            Article.find({})
+            .exec()
+            .then((results) =>{
+                assert(results.length === 3);
+                results.forEach((el,index) =>{
+                    assert(el.name == testingValues[index].name)
+                    assert(el.price == testingValues[index].price)
+                    assert(el.category == testingValues[index].category)
+                })
+                done();
+
+            })
+            .catch(e =>{
+                console.log(e)
+                done();
+            })
         })
     })
 })
